d9d24d26b17fc43c92ccea0590b50485cbf319f8
ctalv <122413805+ctalv@users.noreply.github.com>
2023-05-23 13:38:21 -0500

    added server.js file; added comments on all backed except routes

---
+++ models/index.js
@@ -0,0 +1,7 @@
+// import other models
+
+// User has many Blog
+
+// Blog belongs to one User
+
+// export models
---
+++ server.js
@@ -0,0 +1,11 @@
+// import modules
+
+// create port
+
+// set up handlebars
+
+// create session
+
+// middleware
+
+// start server
