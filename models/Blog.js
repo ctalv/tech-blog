3dd29e4ecf4f73c0dd27f884995725ccc7aa9a86
ctalv <122413805+ctalv@users.noreply.github.com>
2023-05-25 12:43:26 -0500

    added blog model

--- models/Blog.js
+++ models/Blog.js
@@ -1,7 +1,38 @@
-// import Model from sequelize
+// import Model and Datypes class from sequelize
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
 // class constructor
+class Blog extends Model {
+
+}
 
 // define model
+Blog.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }, 
+        text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'blog',
+    }
+)
+
 
 // export model
