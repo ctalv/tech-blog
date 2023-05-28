832682eb68e1e7b378857ffe5405cd71ba7effc5
ctalv <122413805+ctalv@users.noreply.github.com>
2023-05-28 15:20:18 -0500

    added comment seed data and models

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 // import other models
 const User = require('./User');
 const Blog = require('./Blog');
+const Comment = require('./Comment');
 
 // User has many Blog
 User.hasMany(Blog, {
@@ -13,8 +14,19 @@ Blog.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
+// User has many Blog
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+// Blog belongs to one User
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 // export models
-module.exports = { User, Blog };
+module.exports = { User, Blog, Comment };
 
 
 
