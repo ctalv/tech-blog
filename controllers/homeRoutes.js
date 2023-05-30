c748b9d3fe5c8447cd508bbf192fd276c9118dd8
ctalv <122413805+ctalv@users.noreply.github.com>
2023-05-29 21:05:33 -0500

    added helper function

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -14,6 +14,13 @@ router.get('/', async (req, res) => {
       ],
     });
 
+    // handlebars to display blogposts
+    const blogposts = blogData.map((blogpost) => blogpost.get({ plain: true }));
+
+    res.render('homepage', { 
+      blogposts, 
+      logged_in: req.session.logged_in 
+    });
 
     res.status(200).json(blogData);
   } catch (err) {
@@ -27,7 +34,7 @@ router.get('/', async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/dashboard');
     return;
   }
 
